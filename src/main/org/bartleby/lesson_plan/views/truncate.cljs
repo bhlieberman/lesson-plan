0c3cc76cf3b3d836897b9f8ceb1c7176e2f0a8b9
Ben Lieberman <bhlieberman93@gmail.com>
2023-12-02 21:05:38 -0700

    attempts were made w prosemirror

--- src/main/org/bartleby/lesson_plan/views/truncate.cljs
+++ src/main/org/bartleby/lesson_plan/views/truncate.cljs
@@ -1,8 +1,10 @@
 (ns org.bartleby.lesson-plan.views.truncate
-  (:require [goog.string :as gstr]
-            [reagent.core :as r]))
+  (:require
+   [goog.string :as gstr]
+   [reagent.core :as r]))
 
 (defn rewrite-melville []
+  #_[:f> prose-editor]
   (let [text-input (r/atom "")
         original (r/atom "I answered nothing; but effectually dodging every one by the suddenness and rapidity of my flight, rushed from the building, ran up Wall-street towards Broad-way, and jumping into the first omnibus was soon removed from pursuit.")]
     (fn []
@@ -13,8 +15,8 @@
        [:p @text-input]
        [:label.bold.italic.align-self-center {:for "rewrite"} "Write your own version of the above sentence(s) here:"]
        [:textarea.w-50.align-self-center.m-4 {:id "rewrite"
-                   :name "student"
-                   :rows 10
-                   :on-change #(reset! text-input (.. % -target -value))}]
+                                              :name "student"
+                                              :rows 10
+                                              :on-change #(reset! text-input (.. % -target -value))}]
        [:button.btn.btn-primary.w-25.align-self-center {:type :button
-                 :on-click #(reset! original @text-input)} "Rewrite!"]])))
+                                                        :on-click #(reset! original @text-input)} "Rewrite!"]])))
